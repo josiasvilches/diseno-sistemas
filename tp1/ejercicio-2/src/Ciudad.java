624e2d37be83a1cef3364af2405c6ea0e8e6cb76
Franco Vaccarezza <vaco@MacBook-Pro-Vaco.local>
2025-08-27 10:05:19 -0300

    Ejercicio 2

---
+++ tp1/ejercicio-2/src/Ciudad.java
@@ -0,0 +1,18 @@
+public class Ciudad {
+    private boolean esCapitalDeProvincia;
+
+    public Ciudad() {
+    }
+
+    public Ciudad(boolean esCapitalDeProvincia) {
+        this.esCapitalDeProvincia = esCapitalDeProvincia;
+    }
+
+    public boolean isEsCapitalDeProvincia() {
+        return esCapitalDeProvincia;
+    }
+
+    public void setEsCapitalDeProvincia(boolean esCapitalDeProvincia) {
+        this.esCapitalDeProvincia = esCapitalDeProvincia;
+    }
+}
